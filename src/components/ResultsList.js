06a6993ac74f631c6abf7d99b8f4e2a74f271a5a
GlaudistonNeto <glaudistonson@gmail.com>
2019-12-19 23:54:46 -0300

    Added A result for the search, a list of the results withe the name of the place for now

---
+++ src/components/ResultsList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const ResultsList = ({ title, results }) => {
+  return (
+    <View>
+      <Text style={styles.titleStyle}>{title}</Text>
+      <FlatList 
+        horizontal={true}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    marginTop: 10,
+    marginHorizontal: 18,
+    flexDirection: 'row',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
+
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,26 +1,18 @@
 import React, { useState } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async () => {
-    try {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
+  const filterResultsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
     });
-    setResults(response.data.businesses);
-    }catch(err) {
-      setErrorMessage('Samething went wrong...');
-    }
   };
 
   return (
@@ -28,10 +20,14 @@ const SearchScreen = () => {
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
         />
      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+      <ResultsList results={filterResultsByPrice('$$$$')} title="Even Bigger Spender" />
     </View>
   );
 };
